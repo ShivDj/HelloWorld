00dc7a159e7d66b9157c59cae257ed151dc238a8
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-01-30 16:01:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld{
 	public static void main(String args[])
 		{
 			System.out.println("helloworld");
+			System.out.println("edited helloworld");
 			int i=0;
 			for ( i=0; i<=5; i++)
 				System.out.println(i);
